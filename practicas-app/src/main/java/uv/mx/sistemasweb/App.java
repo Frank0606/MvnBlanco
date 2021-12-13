9faeb77b5eb6100a6eafce6be7e54b00aa6c80c5
Francisco <frankdanielsanchez@gmail.com>
2021-12-13 13:38:47 -0600

    Update de la pregunta4, funciona con localhost:4567/aprobado

--- practicas-app/src/main/java/uv/mx/sistemasweb/App.java
+++ practicas-app/src/main/java/uv/mx/sistemasweb/App.java
@@ -1,13 +1,17 @@
 package uv.mx.sistemasweb;
 
-/**
- * Hello world!
- *
- */
+import static spark.Spark.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        get("/aprobado", (req, res) -> {
+
+            return "Espero sacar 10";
+
+        });
+
     }
 }
